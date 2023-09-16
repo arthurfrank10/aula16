1a5b0c765641863c486718f99a2b988c6c5caf5c
arthurfrank10 <142546310+arthurfrank10@users.noreply.github.com>
2023-09-15 21:45:48 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,88 @@
+// this is a js file
+//arthur frank
+
+/* let condicao = true; */
+/*  while (condicao) {
+    //ENQUANTO a condiçao for verdadeira
+    //as linhas de codigo dentro deste bloco
+    //serao executadas
+
+    //assim que a condiçao ficar falsa
+    //o LOOP/LAÇO vai parar
+}
+*/
+/*let i = 0;
+while (i < 10) {
+    console.log(i)
+
+    i++
+}
+
+let estomago = 0;
+while (estomago < 100) {
+    console.log("quero comer mais coxinhas");
+    estomago = estomago + 10;
+}
+*/
+/* let soma = 0;
+let numeros = Number(prompt("digite um numero"));
+
+while (numeros !== 0) {
+    soma = soma + numeros;
+    numeros = Number(prompt("digite um numero pfv"));
+}
+console.log("a soma dos numeros inseridos é" + soma);
+ */
+
+/* const numeros = [14, 67, 89, 15, 23];
+for (let i = 0; i < 5; i++) {
+    const elemento = numeros[i];
+    console.log(elemento); 
+}
+*/
+
+/* const numeros = [1, 2, 3, 4, 5, 6, 7];
+for (let i = 0; i < numeros.length; i++) {
+    console.log("o maior numero é " + numeros[6])
+} */
+
+/* function encontrarMaiorNumero(arr) {
+    let maior = arr[0];
+    for (let i = 0; i < arr.lenght; i++){
+        if (arr[i] > maior){
+            maior = arr[i];
+        }
+    }
+    return "o maior numero é" + maior 
+} */
+/* const numeros2 = [11, 15, 18, 14, 90, 13];
+/* console.log(encontrarMaiorNumero(numeros2));
+ */
+/* let maior = numeros2[0];
+for (let index = 0; index < numeros2.length; index++) {
+    if (numeros2[index] > maior){
+        maior = numeros2[index];
+    }
+    
+}
+console.log(maior); */
+
+/* const numeros = [14, 67, 89, 15, 23];
+for (let numero of numeros){
+    console.log(numero);
+}
+let meuArray = [1, 2, 3, 4, 5];
+meuArray.forEach(elemento => {
+    console.log(elemento);
+}); */
+
+/* let palavras = ["ola", "amigo,", "como", "voce", "esta?"];
+function retornaFraseCompleta(arrayDePalavras){
+    let frase = " ";
+    for (let i = 0; i < arrayDePalavras.length; i++) {
+const elemento = arrayDePalavras[i];
+frase = frase + " " + elemento;
+    }
+    return frase;
+}
+console.log(retornaFraseCompleta(palavras)); */
